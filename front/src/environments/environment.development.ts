8cff69d3c5cdfffc042d7a50ad71e9120226481f
Raishin0 <julian.magnabosco@gmail.com>
2026-07-29 00:32:23 -0300

    intento de docker compose

--- front/src/environments/environment.development.ts
+++ front/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: '/api',
   whatsappNumber: '+5493515479074',
   whatsappLink: 'https://wa.me/5493515479074',
   product_types: [
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: '/api',
   whatsappNumber: '+5493515479074',
   whatsappLink: 'https://wa.me/5493515479074',
   product_types: [
