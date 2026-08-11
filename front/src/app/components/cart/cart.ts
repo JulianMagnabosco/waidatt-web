c89ed2188eb52df5186659c29227350edf7590a0
Raishin0 <julian.magnabosco@gmail.com>
2026-08-10 21:00:49 -0300

    cambios masivos

--- front/src/app/components/cart/cart.ts
+++ front/src/app/components/cart/cart.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { ProductsService } from '../../services/products-service';
@@ -29,13 +29,15 @@ export class Cart implements OnInit {
 
   list = signal<CartItem[]>([]);
 
+  changeDetector = inject(ChangeDetectorRef);
+
   pages = 22;
   elements = 21;
   elementsPerPage = 8;
   actualpage = 0;
   paginationDist = 2;
 
-  liststocks: number[] = [];
+  liststocks =signal<number[]>([]);
   total: number = 0;
 
   ngOnInit(): void {
@@ -64,15 +66,16 @@ export class Cart implements OnInit {
 
     this.service.getCart(data).subscribe({
       next: (value) => {
-        // this.elements=value["elements"]
-        this.list.set(value['list'] as any[]);
+        const elements = value['list'];
+        
         // this.pages=value["pages"]
 
         this.total = 0;
-        console.log(this.list());
-        this.list().forEach((element, index) => {
-          this.searchItem(element.id.toString(), index);
+        elements.forEach((element) => {
+          this.searchItem(element);
         });
+        console.log("listo",this.list());
+        this.list.set(this.list());
       },
       error: (err) => {
         alert('Error inesperado en el servidor, revise su conexion a internet');
@@ -80,21 +83,19 @@ export class Cart implements OnInit {
     });
   }
 
-  searchItem(id: string, index: number) {
-    const quantity = this.list()[index].quantity;
-    this.liststocks.push(quantity);
-    this.service.getProduct(id).subscribe({
+  searchItem(element: any) {
+    const quantity = element.quantity;
+    this.liststocks.update((value) => [...value, quantity]);
+    this.service.getProduct(element.id).subscribe({
       next: (value) => {
         let newItem = value as CartItem;
         // this.list()[index] = newItem;
         // this.list()[index].quantity = quantity;
-        this.list.update((value) => {
-          value[index] = newItem;
-          value[index].quantity = quantity;
-          return value;
-        });
+        newItem.quantity = quantity;
+        this.list.update((value) => [...value, newItem]);
 
         this.total += newItem.price * newItem.quantity;
+        console.log("item",newItem.quantity ,this.total);
       },
       error: (err) => {
         console.log(err);
@@ -104,27 +105,30 @@ export class Cart implements OnInit {
   }
 
   update(index: number) {
-    if (this.liststocks[index] == null) {
+    if (this.liststocks()[index] == null) {
       return;
     }
     let data = {
       id: this.list()[index].id,
-      quantity: this.liststocks[index],
+      quantity: this.liststocks()[index],
     };
     this.service.editCart(data).subscribe({
       next: (value) => {
-        if (this.liststocks[index] <= 0) {
+        if (this.liststocks()[index] <= 0) {
           // this.list.splice(index, 1);
           this.list.update((value) => {
             value.splice(index, 1);
             return value;
           });
-          this.liststocks.splice(index, 1);
+          this.liststocks.update((value) => {
+            value.splice(index, 1);
+            return value;
+          });
           // cAlert("success","Eliminado")
         } else {
           // this.list[index].quantity = this.liststocks[index];
           this.list.update((value) => {
-            value[index].quantity = this.liststocks[index];
+            value[index].quantity = this.liststocks()[index];
             return value;
           });
           // cAlert("success","Añadido al carrito")
@@ -138,10 +142,16 @@ export class Cart implements OnInit {
     });
   }
   notupdate(index: number) {
-    this.liststocks[index] = this.list()[index].quantity;
+    this.liststocks.update((value) => {
+      value[index] = this.list()[index].quantity;
+      return value;
+    });
   }
   remove(index: number) {
-    this.liststocks[index] = 0;
+    this.liststocks.update((value) => {
+      value[index] = 0;
+      return value;
+    });
     this.update(index);
     this.getTotal();
   }
